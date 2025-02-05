d0b6e20494bad26add421d357402fb6bc42d0552
kgrover3 <kgroveriii@gmail.com>
2025-02-04 21:15:39 -0500

    Booleans and logic

--- src/kgrover/Main.java
+++ src/kgrover/Main.java
@@ -1,8 +1,22 @@
 package kgrover;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your age?");
+        int age = scanner.nextInt();
+        System.out.println("Put true or false, are you from Europe?");
+        boolean isEuropean = scanner.nextBoolean();
+
+        int euDrinkingAge = 18;
+        int usDrinkingAge = 21;
+
+        boolean canDrink = isEuropean && age >= euDrinkingAge || !isEuropean && age >= usDrinkingAge;
+        System.out.println("Can you drink? " + canDrink);
 
     }
 }
